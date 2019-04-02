5e2db9f3375252b2b56ea7f232d97c8e5a57c62e
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-04-02 17:53:41 -0500

    Added move to front

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -57,38 +57,94 @@ class DoublyLinkedList:
             self.head = new_node
             self.tail = new_node
         else:
-            # currentHead = self.head
-            # new_node = ListNode(value, None, currentHead)
-            # self.head = new_node
-            self.head.prev = new_node
-            self.head.prev.next = self.head
-            self.head = new_node
+            self.head.insert_before(value)
+            self.head = self.head.prev
         self.length += 1
-        print("Head:", new_node)
-        # return self.length
 
-    # def remove_from_head(self):
-    #     oldHead = self.head
-    #     if self.length <= 1:
-    #         self.head = None
-    #         self.tail = None
-    #     else:
-    #         self.head = oldHead.next
-    #         self.head.prev = None
-    #         oldHead.next = None
-    #         # followingHead = self.head.next
-    #         # self.head = followingHead
-    #     self.length -= 1
-    #     return self.length
+    def remove_from_head(self):
+        oldHead = self.head
+        if self.length <= 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head.delete()
+        self.length -= 1
+        return oldHead.value
 
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.insert_after(value)
+            self.tail = self.tail.next
+        self.length += 1
+        return self.tail.value
 
     def remove_from_tail(self):
-        pass
+        to_be_removed = self.tail
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail.delete()
+        self.length -= 1
+        return to_be_removed.value
 
     def move_to_front(self, node):
-        pass
+        current = self.head
+        new_node = ListNode(node)
+        count = 0
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            return self.head.value
+        elif self.length == 2:
+            temporary = self.head.value
+            self.head.value = self.tail.value
+            self.tail.value = temporary
+        else:
+            while current.value != new_node.value:
+                if count == self.length:
+                    return None
+                temp = self.head.value
+                self.head.value = current.next.value
+                self.head.next.value = temp
+                count += 1
+            temp = current
+            self.head.insert_before(temp.value)
+            current.next = current.prev
+            current.delete()
+            self.head = self.head.prev
+        return self.head.value
+
+    # def move_to_front(self, node):
+    #     if self.head == None:
+    #         return 'List Empty'
+    #     else:
+    #         curr_node = self.head
+    #         while True:
+    #             # if curr_node matches passed in node
+    #             if curr_node == node:
+    #                 # self.add_to_head(curr_node.value)
+    #                 self.head.insert_before(node)
+    #                 # delete curr_node
+    #                 curr_node.delete()
+    #             # else:
+    #             else:
+    #                 # curr_node = curr_node.next
+    #                 curr_node = curr_node.next
+
+    def print(self):
+        arr = []
+        current = self.head
+        while current:
+            arr.append(current.value)
+            current = current.next
+        print('Arr:', arr)
 
     def move_to_end(self, node):
         pass
@@ -100,14 +156,27 @@ class DoublyLinkedList:
         pass
 
 
-my_test = DoublyLinkedList(10)
+my_value = ListNode(100)
+my_test = DoublyLinkedList(my_value)
+
 
 # my_node = ListNode(14)
 
 # print(my_node.insert_after(180))
 # print(my_node.prev)
-print(my_test.add_to_head(180))
+# print(my_test.add_to_head(275))
 # my_test.add_to_head(280)
 # my_test.add_to_head(2800)
-print(my_test.remove_from_head())
+# print(my_test.remove_from_head())
 # print(my_test.add())
+my_test.add_to_tail(3)
+my_test.add_to_head(29)
+# print(my_test.add_to_tail(1080))
+# print(my_test.add_to_tail(293))
+# print(my_test.add_to_tail(1340))
+
+# print(my_test.remove_from_tail())
+
+print(my_test.move_to_front(100))
+
+# print(my_test.print())
